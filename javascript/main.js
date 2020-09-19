c4645f08d3fe01f013c51826158cfebf9177db0f
Jeong-hansol <jhs486674@gmail.com>
2020-09-19 16:17:07 +0900

    morepage script

--- javascript/main.js
+++ javascript/main.js
@@ -12,3 +12,39 @@ var swiper = new Swiper('.swiper-container', {
         prevEl: '.swiper-button-prev',
       },
 });
+
+
+// 포트폴리오 view more 페이지
+$(".pulse-button.btn.btn1").click(function (evt) {
+    evt.preventDefault();
+    $(".morepage01").fadeIn();
+});
+
+$(".pulse-button.btn.btn2").click(function (evt) {
+    evt.preventDefault();
+    $(".morepage02").fadeIn();
+});
+
+$(".pulse-button.btn.btn3").click(function (evt) {
+    evt.preventDefault();
+    $(".morepage03").fadeIn();
+});
+
+$(".pulse-button.btn.btn4").click(function (evt) {
+    evt.preventDefault();
+    $(".morepage04").fadeIn();
+});
+
+$(".close,.morepage").click(function () {
+    $(".morepage").fadeOut();
+});
+
+$(document).keyup(function (evt) {
+    if (evt.which == 27) { $(".morepage").hide(); };
+}); // ESC 키로 닫기
+
+$(".morepage .more_cont").on({
+    "click": function (evt) {
+        evt.stopPropagation();
+    }
+}); // 창 밖 클릭 시에만 화면 닫기
